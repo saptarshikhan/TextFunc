b6361656f7d6ad3aa1ab4cd689d05d7c0efc3357
saptarshikhan <106461369+saptarshikhan@users.noreply.github.com>
2022-12-14 00:54:53 +0530

    Add files via upload
    Footer.js added

---
+++ src/components/Footer.js
@@ -0,0 +1,103 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+       
+<div className="container my-5">
+
+  
+  <footer
+          className="text-center text-lg-start text-white"
+          style="background-color: #1c2331"
+          >
+   
+    <section
+             className="d-flex justify-content-between p-4"
+             style="background-color: #6351ce"
+             >
+      
+      <div className="me-5">
+        <span>Get connected with me on social networks:</span>
+      </div>
+      
+
+      
+      <div>
+        <a href="https://www.facebook.com/profile.php?id=100073702355617" className="text-white me-4">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://twitter.com/itsme_saptarshi" className="text-white me-4">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="http://google.com" className="text-white me-4">
+          <i className="fab fa-google"></i>
+        </a>
+        <a href="https://www.instagram.com/s_for_saptarshi/" className="text-white me-4">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/saptarshi-khan-2b7bb9247/" className="text-white me-4">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/saptarshikhan" className="text-white me-4">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
+     
+    </section>
+    
+
+    
+    <section className="">
+      <div className="container text-center text-md-start mt-5">
+       
+        <div className="row mt-3">
+          
+          <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+           
+            <h6 className="text-uppercase fw-bold">Created By</h6>
+            <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+             Saptarshi Khan. A undergraduate student from National Institute of Technology, Durgapur.
+            </p>
+          </div>
+          
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+           
+            <h6 className="text-uppercase fw-bold">Contact</h6>
+            <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+            <p><i className="fas fa-envelope mr-3"></i> saptarshikhan@gmail.com</p>
+            <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+            <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </div>
+          
+        </div>
+        
+      </div>
+    </section>
+   
+    <div
+         className="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a className="text-white" href="https://mdbootstrap.com/"
+         >Saptarshi_khan.com</a
+        >
+    </div>
+  
+  </footer>
+  
+
+</div>
+
+    </div>
+  )
+}
